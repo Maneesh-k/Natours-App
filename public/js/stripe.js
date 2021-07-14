fc1b68cb2445a806d8cdd9b77f874be01e201e39
Maneesh k <77183545+Maneesh-k@users.noreply.github.com>
2021-07-14 16:44:39 +0530

    Update stripe.js

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import axios from 'axios';
 import { showAlert } from './alerts';
-const stripe = Stripe(process.env.STRIPE_KEY);
+const stripe = Stripe('pk_test_51j5mUUSGRRMcwQbcvl1jAPZ5u0le5f4as1GRD3ZCnteiOBJU2nN2Ouw2f5q');
 
 export const bookTour = async tourId => {
   try {
